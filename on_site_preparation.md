e2c61a69d349a2c2973e6fc2e893879734af1c59
rabbi50 <jasrabbi50@gmail.com>
2022-12-24 19:21:16 +0600

    on_site_preparation_done

---
+++ on_site_preparation.md
@@ -0,0 +1,20 @@
+# For candidate
+
+1. Practice writing code without computer
+2. Go over general sample problem
+3. Ask your friends to test you
+4. Try to ask what the style of technical questions will be
+
+
+# For interviewer
+
+1. Make sure questions are relevent to the role
+2. Have backup interviewers
+3. Book The room for the day
+4. Allow time for the breaks
+
+# On site topics
+
+1. String.String methods. What is string?
+2. Reverse a String
+3. Array. Array methods
--- phone_screen_problem/main.js
+++ phone_screen_problem/main.js
@@ -11,38 +11,70 @@
   - Time limit: 30 minutes
 */
 
-
 /* DO NOT MODIFY */
-const rootElement = document.querySelector('.foods');
+const rootElement = document.querySelector(".foods");
 
 const foodData = [
   {
     id: 1,
-    image: '🌮',
-    name: 'taco'
+    image: "🌮",
+    name: "taco",
   },
   {
     id: 2,
-    image: '🍔',
-    name: 'burger'
+    image: "🍔",
+    name: "burger",
   },
   {
     id: 3,
-    image: '🍆',
-    name: 'eggplant'
+    image: "🍆",
+    name: "eggplant",
   },
   {
     id: 4,
-    image: '🍎',
-    name: 'apple'
+    image: "🍎",
+    name: "apple",
   },
   {
     id: 5,
-    image: '🥞',
-    name: 'pancakes'
+    image: "🥞",
+    name: "pancakes",
   },
 ];
 /* DO NOT MODIFY */
 
-
 /** YOUR CODE BELOW **/
+
+class Foods {
+  constructor(el, foodData) {
+    this._root = el;
+    this._data = foodData;
+  }
+
+  renderList() {
+    this._root.addEventListener("click", (event) => {
+      const { target } = event;
+      target.remove();
+    });
+
+    const fragment = document.createDocumentFragment();
+
+    this._data.forEach((i) => {
+      fragment.appendChild(this.createFoodItem(i));
+    });
+
+    this._root.appendChild(fragment);
+  }
+
+  createFoodItem(item) {
+    const itemEl = document.createElement("div");
+    itemEl.innerText = item.image;
+    itemEl.classList.add(item.name);
+
+    return itemEl;
+  }
+}
+
+
+const foods = new Foods(rootElement, foodData);
+foods.renderList();
