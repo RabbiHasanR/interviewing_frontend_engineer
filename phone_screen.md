7c97b5c51a017992c1e29c75f0e7c02e96e340a6
rabbi50 <jasrabbi50@gmail.com>
2022-12-24 18:37:09 +0600

    phone_screen question

---
+++ phone_screen.md
@@ -0,0 +1,13 @@
+# For candidate
+
+1. Ask questions
+2. Talk out your solution
+3. Get comfortable with the environment
+
+
+# For the interviewer
+
+1. Are you in a quite area?
+2. Is the problem well worded?
+3. Does the candidate know what the requirements and restrictions are?
+4. Did you leave time for quetions at the end?
---
+++ phone_screen_problem/main.js
@@ -0,0 +1,48 @@
+/*
+  Hi there! Thanks for taking on this code test. The requirements are listed below:
+  
+  1. Create a "Foods" class or constructor that will take two arguements: a root element and a data object (foodData).
+  2. Render all of the items in the data object into the DOM with the root element as the parent
+  3. If the user clicks a food item, it should be removed from the list
+  
+  Rules:
+  - Only vanilla JS
+  - Feel free to use Google, Bing, DuckDuckGo to look things up
+  - Time limit: 30 minutes
+*/
+
+
+/* DO NOT MODIFY */
+const rootElement = document.querySelector('.foods');
+
+const foodData = [
+  {
+    id: 1,
+    image: '🌮',
+    name: 'taco'
+  },
+  {
+    id: 2,
+    image: '🍔',
+    name: 'burger'
+  },
+  {
+    id: 3,
+    image: '🍆',
+    name: 'eggplant'
+  },
+  {
+    id: 4,
+    image: '🍎',
+    name: 'apple'
+  },
+  {
+    id: 5,
+    image: '🥞',
+    name: 'pancakes'
+  },
+];
+/* DO NOT MODIFY */
+
+
+/** YOUR CODE BELOW **/
